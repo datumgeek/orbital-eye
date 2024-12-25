9596c96264ab68a68f394cbbebe5fe5bdb81a9b7
Mike Graham <datumgeek@yahoo.com>
2024-12-25 03:22:44 -0700

    Updates

---
+++ _next/static/dVJ-IFsVgGZlVNeutUn-b/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-8c3819c16031c421.js"],"/_error":["static/chunks/pages/_error-fdc5cf1f34cb592f.js"],"/portal":["static/chunks/pages/portal-ae29ac70d1c74637.js"],sortedPages:["/","/_app","/_error","/portal"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
