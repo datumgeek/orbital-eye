168e9004f06a674f8be641c6ced6c5ef24458ab4
Mike Graham <datumgeek@yahoo.com>
2025-01-05 02:53:46 -0700

    fix(@orbital-eye/e02-visualize): cleanup unused imports in jotai data host

--- libs/orbital-eye/e02-visualize/jotai-data-host/jotai-data-host.tsx
+++ libs/orbital-eye/e02-visualize/jotai-data-host/jotai-data-host.tsx
@@ -1,9 +1,6 @@
-import { ReactNode, useEffect } from 'react';
-import styles from './jotai-data-host.module.scss';
-import { atom, useSetAtom } from 'jotai';
-import { satelliteDataAtom, useLoadSatelliteData } from './data/satellite-data';
+import { ReactNode } from 'react';
+import { useLoadSatelliteData } from './data/satellite-data';
 import {
-  conjunctionForecastAtom,
   useLoadConjunctionData,
 } from './data/public-conjunction-data';
 
