de5a7ae9d5d98a01f22bfcd1e1906e74230aeb47
Mike Graham <datumgeek@yahoo.com>
2025-01-05 04:40:16 -0700

    feat(@orbital-eye/e02-visualize): implement click on satellite point cloud to show satellite details

--- libs/orbital-eye/e02-visualize/time-slice-viz/time-slice-viz.tsx
+++ libs/orbital-eye/e02-visualize/time-slice-viz/time-slice-viz.tsx
@@ -5,13 +5,19 @@ import { TextureLoader } from 'three';
 import { OrbitControls, Sphere } from '@react-three/drei';
 import * as satellite from 'satellite.js';
 import { useAtom } from 'jotai';
-import { satelliteDataAtom } from '../jotai-data-host/data/satellite-data';
+import {
+  SatelliteData,
+  satelliteDataAtom,
+} from '../jotai-data-host/data/satellite-data';
+import { useShellDispatch } from '@porrtal/r-shell';
 
 const Earth = () => {
   const basePath = window.location.pathname.startsWith('/orbital-eye')
-  ? '/orbital-eye/'
-  : '/';
-const texture = new TextureLoader().load(`${basePath}docs/images/ne1-small.png`);
+    ? '/orbital-eye/'
+    : '/';
+  const texture = new TextureLoader().load(
+    `${basePath}docs/images/ne1-small.png`
+  );
   return (
     <mesh>
       <sphereGeometry args={[6.371, 64, 64]} />
@@ -20,13 +26,36 @@ const texture = new TextureLoader().load(`${basePath}docs/images/ne1-small.png`)
   );
 };
 
-const SatellitePointCloud = (props: { positions: Float32Array }) => {
-  const [satelliteData] = useAtom(satelliteDataAtom);
+const SatellitePointCloud = (props: {
+  positions: Float32Array;
+  satelliteData: SatelliteData[];
+}) => {
+  const shellDispatch = useShellDispatch();
 
-  // console.log('positions: ', props.positions);
+  const handleClick = (event: any) => {
+    event.stopPropagation();
+
+    // Find the clicked point
+    const index = Math.floor(event.index / 3); // Divide by 3 because each satellite position has x, y, z
+    const satelliteInfo = props.satelliteData[index];
+
+    if (satelliteInfo) {
+      // alert(
+      //   `Satellite Info:\nName: ${satelliteInfo.OBJECT_NAME}\nTLE Line 1: ${satelliteInfo.TLE_LINE1}\nTLE Line 2: ${satelliteInfo.TLE_LINE2}`
+      // );
+      shellDispatch({
+        type: 'launchView',
+        viewId: 'SatelliteDetails',
+        state: {
+          noradCatId: satelliteInfo.NORAD_CAT_ID,
+          objectName: satelliteInfo.OBJECT_NAME as string,
+        },
+      });
+    }
+  };
 
   return (
-    <points>
+    <points onClick={handleClick}>
       <bufferGeometry>
         <bufferAttribute
           attach="attributes-position"
@@ -43,6 +72,9 @@ const SatellitePointCloud = (props: { positions: Float32Array }) => {
 export const TimeSliceViz = () => {
   const [satelliteData] = useAtom(satelliteDataAtom);
   const [positions, setPositions] = useState<Float32Array>();
+  const [satelliteCloudData, setSatelliteCloudData] = useState<SatelliteData[]>(
+    []
+  );
 
   useEffect(() => {
     if (!satelliteData) {
@@ -50,6 +82,7 @@ export const TimeSliceViz = () => {
     }
 
     const satPositions: number[] = [];
+    const satData: SatelliteData[] = [];
     const date = new Date();
 
     satelliteData.forEach((sat) => {
@@ -58,15 +91,12 @@ export const TimeSliceViz = () => {
       if (!tle1 || !tle2) {
         return;
       }
-      
+
       const satrec = satellite.twoline2satrec(tle1, tle2);
       const positionAndVelocity = satellite.propagate(satrec, date);
 
       const pos = positionAndVelocity.position;
-      if (
-        pos &&
-        typeof pos !== 'boolean'
-      ) {
+      if (pos && typeof pos !== 'boolean') {
         const gmst = satellite.gstime(date);
         const eciPos = satellite.eciToEcf(
           positionAndVelocity.position as satellite.EciVec3<number>,
@@ -74,23 +104,29 @@ export const TimeSliceViz = () => {
         );
 
         // Convert to XYZ and scale down
-        const x = (eciPos.x / 1000);
-        const y = (eciPos.y / 1000);
-        const z = (eciPos.z / 1000);
+        const x = eciPos.x / 1000;
+        const y = eciPos.y / 1000;
+        const z = eciPos.z / 1000;
 
         // console.log('satellite', {x, y, z});
 
         satPositions.push(x, z, y); // Adjust axes for visualization
+        satData.push(sat);
       }
     });
 
     setPositions(new Float32Array(satPositions));
+    setSatelliteCloudData(satData);
   }, [satelliteData]);
 
-
   // console.log('positions: ', positions);
-  if (!satelliteData || satelliteData.length === 0 || !positions || positions.length === 0) {
-    return (<div>loading...</div>);
+  if (
+    !satelliteData ||
+    satelliteData.length === 0 ||
+    !positions ||
+    positions.length === 0
+  ) {
+    return <div>loading...</div>;
   }
 
   return (
@@ -101,7 +137,10 @@ export const TimeSliceViz = () => {
       <ambientLight intensity={0.9} />
       <pointLight position={[10, 10, 10]} />
       <Earth />
-      <SatellitePointCloud positions={positions} />
+      <SatellitePointCloud
+        positions={positions}
+        satelliteData={satelliteData}
+      />
       <OrbitControls />
     </Canvas>
   );
